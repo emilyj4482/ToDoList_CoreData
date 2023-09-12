23782c80aa492d5910e6319418b9782c81b03617
emilyj4482 <emilyj4482@gmail.com>
2023-09-12 14:21:15 +0900

    first commit

---
+++ ToDoList_CoreData/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  ToDoList_CoreData
+//
+//  Created by EMILY on 2023/09/12.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("ToDoList")
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ ToDoList_CoreData/ToDoList_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_CoreDataApp.swift
+//  ToDoList_CoreData
+//
+//  Created by EMILY on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
